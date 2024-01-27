2af96274ca4ac0824be37794bff069b3ddd46b1c
Dennis Jung <dmjung@live.de>
2024-01-27 13:01:56 +0100

    shadow now detects player entered into its hunting area

--- scenes/enemies/shadow/debug_overlay.gd
+++ scenes/enemies/shadow/debug_overlay.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
+var player: CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	var screen_centre = get_viewport().size/2
-	DebugDraw2D.line(global_position, Vector2(1,2))
+	if player:
+		DebugDraw2D.line(global_position, player.global_position)
 	
-
-
-func _on_hunting_area_area_entered(area):
-	pass # Replace with function body.
+func _on_hunting_area_body_entered(body):
+	if body.name == "PlayerCharacter":
+		player = body
+	
+func _on_hunting_area_body_exited(body):
+	if body.name == "PlayerCharacter":
+		player = null
