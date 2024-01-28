a1a597443f60187fc025728201091358d7ab0f79
Dennis Jung <35849153+dennis-jung@users.noreply.github.com>
2024-01-28 08:27:53 +0100

    Castle now plays once, if player enters area

--- scenes/castle/castle.gd
+++ scenes/castle/castle.gd
@@ -9,17 +9,11 @@ func _ready():
 	pass # Replace with function body.
 
 
-func _process(_delta):
-	if is_game_over:
-		_animated_sprite.play("open")
-	else:
-		_animated_sprite.stop()
-
-
 func _on_area_2d_body_entered(body):
 	# check if it's the player
 	if body.get_name() == "PlayerCharacter":
 		print("Open!")
+		_animated_sprite.play("open")
 		is_game_over = true
 
 
